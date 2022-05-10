7f325190f695ac4a26a05056c984879005f4a9bd
Everett Blakley <39717736+everettblakley@users.noreply.github.com>
2022-05-10 11:36:43 -0600

    Adding dialog component

---
+++ components/ui/Dialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition name="slide-fade" appear>
+    <div v-show="show" class="rounded border border-gray-400 bg-white p-3">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "UiDialog",
+  props: {
+    show: Boolean,
+  },
+};
+</script>
+
+<style>
+.slide-fade-enter,
+.slide-fade-leave-to {
+  @apply scale-90 opacity-0;
+}
+
+.slide-fade-enter-to,
+.slide-fade-leave {
+  @apply scale-100 opacity-100;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  @apply transition;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,42 @@
 <template>
-  <Tutorial/>
+  <main class="h-screen w-screen flex items-center justify-center">
+    <button class="btn" @click="toggleDialog">Open Dialog</button>
+    <ui-dialog
+      :show="showDialog"
+      class="fixed top-10 left-1/2 transform -translate-x-1/2"
+    >
+      <article class="space-y-4">
+        <h1 class="text-xl font-bold">I am the dialog</h1>
+        <input
+          class="border-gray-400 border placeholder-gray-400 rounded px-3 py-2 w-full"
+          placeholder="I am an input"
+        />
+        <button class="btn" @click="toggleDialog">Close Dialog</button>
+      </article>
+    </ui-dialog>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return { showDialog: false };
+  },
+  methods: {
+    toggleDialog() {
+      this.showDialog = !this.showDialog;
+    },
+  },
+};
 </script>
+
+<style>
+.btn {
+  @apply bg-blue-600 text-white font-semibold rounded px-3 py-2;
+}
+
+.btn:focus {
+  @apply outline-none ring-2 ring-offset-1 ring-blue-600;
+}
+</style>
